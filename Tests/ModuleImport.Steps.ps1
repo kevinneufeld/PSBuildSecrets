259be41017527c1370deb1848cc8878321972fdc
Stefan Henseler <stefan.henseler@synax.ch>
2017-12-29 16:08:11 +0100

    Updates Test Spec

--- Tests/ModuleImport.Steps.ps1
+++ Tests/ModuleImport.Steps.ps1
@@ -1,23 +1,37 @@
-BeforeEachFeature {
-    
-    $Script:ModuleName = "PSBuildSecrets"
-
-    $Script:ModuleManifestPath = Join-Path "$($PSScriptRoot)\..\Source\" "$ModuleName.psd1"
-}
 
 Given 'the module manifest exists' {
+    
+    $ModuleName = "PSBuildSecrets"
+
+    $ModuleManifestPath = Join-Path "$($PSScriptRoot)\..\Source\" "$ModuleName.psd1"
+
     $ModuleManifestPath | Should Exist
 }
 
 Given 'the module manifest is valid' {
+
+    $ModuleName = "PSBuildSecrets"
+
+    $ModuleManifestPath = Join-Path "$($PSScriptRoot)\..\Source\" "$ModuleName.psd1"
+
     Test-ModuleManifest -Path $ModuleManifestPath | Should Be $true
 }
 
 When 'we try to import the module' {
+
+    $ModuleName = "PSBuildSecrets"
+
+    $ModuleManifestPath = Join-Path "$($PSScriptRoot)\..\Source\" "$ModuleName.psd1"
+
     {Import-Module $ModuleManifestPath }| Should Not Throw
 }
 
 Then 'the module is loaded without any exceptions' {
+    
+    $ModuleName = "PSBuildSecrets"
+
+    $ModuleManifestPath = Join-Path "$($PSScriptRoot)\..\Source\" "$ModuleName.psd1"
+
     Get-Module -Name $ModuleName | Should Not Be $false
 }
 
